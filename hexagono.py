a51553190df933e12a85bf1cf8f8a5867d67569f
AugustoBonifacio001 <147618471+AugustoBonifacio001@users.noreply.github.com>
2023-11-08 09:42:16 -0300

    Update hexagono.py

--- hexagono.py
+++ hexagono.py
@@ -2,11 +2,11 @@ import math
 
 # calcula a area de um hexagono regular
 def area(lado):
-    #
-    #
-    return
+    area =(6*lado**2.math(3))/4
+    
+    return area
 
 # calcula o perimetro de um hexagono regular
 def perimetro(lado):
-    #
-    return
+    perimetro=6*lado
+    return perimetro
