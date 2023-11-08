be2e030d342042c8c4f68fcc04dcfb74363dca6b
Marcelo Fiuza Pereira <marcelo.pereira2@estudante.ufla.br>
2023-11-08 14:11:39 -0300

    corrigido o nome das funções

--- hexagono.py
+++ hexagono.py
@@ -1,12 +1,12 @@
 import math
 
 # calcula a area de um hexagono regular
-def areaHexagono(lado):
+def area(lado):
     areaHexagono = 6 * (((lado*lado) * math.sqrt(3)) / 4 )
     return areaHexagono
 
 # calcula o perimetro de um hexagono regular
-def perimetroHexagono(lado):
+def perimetro(lado):
     perimetroHexagono = 6 * lado
     return perimetroHexagono
 
