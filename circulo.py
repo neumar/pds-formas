be2389253daa80368807b7619859d2778c022796
GabrielHush14 <147618401+GabrielHush14@users.noreply.github.com>
2023-11-10 14:00:39 +0000

    fiz a area e comprimento do circulo top demais!!!

--- circulo.py
+++ circulo.py
@@ -2,10 +2,10 @@ Pi = 3.1415
 
 # calcula a area de um circulo
 def area(raio):
-    #
-    return
+    area=Pi*raio*raio
+    return area
 
 #calcula o comprimenot do circunferencia
 def comprimento_circunferencia(raio):
-    #
-    return
+    comprimento_circunferencia=2*Pi*raio
+    return comprimento_circunferencia
