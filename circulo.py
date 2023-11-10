15766982a6b1fabe117f3c0dce4a20460eded407
GabrielHush14 <147618401+GabrielHush14@users.noreply.github.com>
2023-11-10 11:05:42 -0300

    Update circulo.py

--- circulo.py
+++ circulo.py
@@ -1,5 +1,8 @@
 Pi = 3.1415
-
+#primeiro instalei o teste no terminal.
+#depois fiz os codigos.
+#dei um checkout e testei o codigo no qual passou
+#depois commitei e dei pull
 # calcula a area de um circulo
 def area(raio):
     area=Pi*raio*raio
